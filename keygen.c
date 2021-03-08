fb6e6446e1a17bc24d6d085e82fbad80f6a98258
lorenzcompagnucci <lorenz.compagnucci@studenti.unicam.it>
2021-03-08 17:31:20 +0100

    FINALE

--- keygen.c
+++ keygen.c
@@ -1,31 +1,69 @@
 #include <stdio.h>
 #include <string.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 #include "keygen.h"
 #include "file.h"
 
 void create_key(char* path, char* alfabeto, char* sostituto, char* speciale, char* chiave) {
-    assert(check_alphabet(alfabeto) == 1);
+    char* alphabet = check_alphabet(alfabeto);
+    char replace_char = check_char(sostituto, REPLACEMENT_CHAR);
+    char special_char = check_char(speciale, SPECIAL_CHAR);
+    char* keyword = check_key(chiave);
     FILE* fp = fopen(path, "w");
     check_file(fp, OUT_KEY_ERROR);
-    fprintf(fp, "%s\r\n%c\r\n%c\r\n%s\r\n", alfabeto, sostituto[0], speciale[0], chiave);
+    fprintf(fp, "%s\r\n%c\r\n%c\r\n%s\r\n", alphabet, replace_char, special_char, keyword);
     fflush(fp);
     fclose(fp);
 }
 
-int check_alphabet(char* alfabeto) {
-    if (strlen(alfabeto) != 25) {
+char* check_alphabet(char* alphabet) {
+    if (strlen(alphabet) != 25) {
         printf("THE ALPHABET MUST BE MADE OF 25 CHARS.\n");
-        return -1;
+        exit(0);
     }
-    for (int i = 0; i < 25; ++i) {
-        for (int j = 0; j < 25; ++j) {
-            if (j != i && alfabeto[i] == alfabeto[j]) {
-                printf("CHARACTER '%c' IS PRESENT MORE THAN ONCE.\n", alfabeto[i]);
-                return -1;
-            }
+    int check[25];
+    for (int i = 0; i < 25; i++) {
+        check[i] = 0;
+        check_reps(check, alphabet[i], i);
+    }
+    return alphabet;
+}
+
+void check_reps(int* check, char c, int i) {
+    char a = toupper(c);
+    if (a < 'A' || a > 'Z') {
+        printf("A CHARCTER OF THE ALPHABET IS NOT A LETTER.\n");
+        exit(0);
+    }
+    if (check[i] == 1) {
+        printf("THE ALPHABET HAS TO CONTAIN 25 DIFFERENT CHARACTERS.\n");
+        exit(0);
+    }
+    check[i] = 1;
+}
+
+char check_char(char* line, char* type_char) {
+    if (strlen(line) != 1) {
+        printf("%s HAS TO BE JUST ONE CHAR.\n", type_char);
+        exit(0);
+    }
+    char c = toupper(line[0]);
+    if (c < 'A' || c > 'Z') {
+        printf("%s IS NOT A CHARACTER.\n", type_char);
+        exit(0);
+    }
+    return c;
+}
+
+char* check_key(char* keyword) {
+    for (int i = 0; i < strlen(keyword); i++) {
+        char c = toupper(keyword[i]);
+        if (c < 'A' || c > 'Z') {
+            printf("A CHAR OF THE KEYWORD IS NOT A LETTER.\n");
+            exit(0);
         }
     }
-    return 1;
+    return keyword;
 }
--- keygen.h
+++ keygen.h
@@ -2,8 +2,13 @@
 #define PLAYFAIR_KEYGEN_H
 
 #define OUT_KEY_ERROR "ERROR WHILE CREATING THE NEW KEYFILE"
+#define REPLACEMENT_CHAR "REPLACEMENT CHAR"
+#define SPECIAL_CHAR "SPECIAL CHAR"
 
 void create_key(char* path, char* alfabeto, char* sostituto, char* speciale, char* chiave);
-int check_alphabet(char* alfabeto);
+char* check_alphabet(char* alphabet);
+char check_char(char* line, char* type_char);
+void check_reps(int* check, char c, int i);
+char* check_key(char* keyword);
 
 #endif //PLAYFAIR_KEYGEN_H
